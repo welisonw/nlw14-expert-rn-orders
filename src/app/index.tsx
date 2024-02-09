68cd0bf5f094fd8ad5ef2c4a7a5764e2b9527a30
Welison Wessling <wwelisonw@gmail.com>
2024-02-09 12:38:27 -0300

    feat: added View with marginBottom around the Header component in Home

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -58,7 +58,9 @@ export default function Home() {
 
 	return (
 		<View className='flex-1 pt-8'>
-			<Header title='Faça seu pedido' cartQuantityItems={cartQuantityItems} />
+			<View className='mx-5'>
+        <Header title='Faça seu pedido' cartQuantityItems={cartQuantityItems} />
+      </View>
 
 			<FlatList
 				data={CATEGORIES}
