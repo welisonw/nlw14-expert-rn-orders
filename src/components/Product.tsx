67a7c0d796236daed69eb94e841f20d89d32dc4b
Welison Wessling <wwelisonw@gmail.com>
2024-02-09 12:41:47 -0300

    feat: added deleteButton property to delete product in Product component

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -8,23 +8,29 @@ import {
 	View,
 } from 'react-native';
 
+import { Feather } from '@expo/vector-icons';
+
+import colors from 'tailwindcss/colors';
+
 interface ProductDataProps {
 	thumbnail: ImageProps;
 	title: string;
 	description: string;
+	quantity?: number;
 }
 
 interface ProductProps extends TouchableOpacityProps {
 	data: ProductDataProps;
+	deleteButton?: () => void;
 }
 
 export const Product = forwardRef<TouchableOpacity, ProductProps>(
-	({ data, ...props }, ref) => {
+	({ data, deleteButton, ...props }, ref) => {
 		return (
 			<TouchableOpacity
 				className='w-full flex-row items-center pb-4 space-x-3'
 				activeOpacity={0.7}
-        ref={ref}
+				ref={ref}
 				{...props}
 			>
 				<Image
@@ -33,10 +39,30 @@ export const Product = forwardRef<TouchableOpacity, ProductProps>(
 					className='w-[92px] h-[92px] rounded-md'
 				/>
 
-				<View className='flex-1 space-y-[2px]'>
-					<Text className='font-subtitle text-base text-slate-100'>
-						{data.title}
-					</Text>
+				<View className='flex-1 space-y-1'>
+					<View className='flex-row items-center'>
+						<Text className='flex-1 font-subtitle text-base text-slate-100'>
+							{data.title}
+						</Text>
+
+						{deleteButton && (
+							<TouchableOpacity onPress={deleteButton}>
+								<Feather
+									name='trash-2'
+									size={19}
+									color={colors.red[500]}
+									style={{ padding: 2 }}
+								/>
+							</TouchableOpacity>
+						)}
+					</View>
+
+					{data.quantity && (
+						<Text className='font-subtitle text-sm text-slate-400'>
+							quantidade: {data.quantity}
+						</Text>
+					)}
+
 					<Text className='font-normal text-xs text-slate-400 leading-5'>
 						{data.description}
 					</Text>
